c5996021a0a5a6296e1ae4f3e6d9d9b5bc90fffb
mistrain <mahdimousavi40@gmail.com>
2023-09-01 16:59:03 +0330

    add login fn and redirect to home

--- website/views.py
+++ website/views.py
@@ -1,12 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
-from django.contrib import messages 
+from django.contrib import messages
 # Create your views here.
-def home(request):
-    return render(request, 'home.html', {})
 
-def login_user(request):
-    pass
+
+def home(request):
+    print(request.user.is_authenticated)
+
+    # check to see if logging in
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, 'You have been logged in')
+            return redirect('home')
+        else:
+            messages.success(request, 'There was an Error logging in, please try again')
+            return redirect('home')
+
+    else:
+        return render(request, 'home.html', {})
+
 
 def logout_user(request):
-    pass
+    logout(request)
+    messages.success(request, "You have been logged out!")
+    return redirect('home')
