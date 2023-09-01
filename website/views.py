1ba364f5825c1eb7f2991bc405346e6c8a46cc5a
mistrain <mahdimousavi40@gmail.com>
2023-09-01 18:14:10 +0330

    bug fix & register fn set & pass to register view

--- website/views.py
+++ website/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from .forms import SignUpForm
 # Create your views here.
 
 
@@ -18,7 +19,8 @@ def home(request):
             messages.success(request, 'You have been logged in')
             return redirect('home')
         else:
-            messages.success(request, 'There was an Error logging in, please try again')
+            messages.success(
+                request, 'There was an Error logging in, please try again')
             return redirect('home')
 
     else:
@@ -32,4 +34,19 @@ def logout_user(request):
 
 
 def register_user(request):
-    return render(request, 'register.html', {})
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            # Authenticate and log in
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(
+                request, "You have successfully registerd, Welcome!")
+            return redirect('home')
+    else:
+        form = SignUpForm()
+        return render(request, 'register.html', {'form': form})
+    return render(request, 'register.html', {'form': form})
