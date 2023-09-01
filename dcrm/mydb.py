a3c362c4aeab51b58bfd7dc763514e5eb9aa6688
mistrain <mahdimousavi40@gmail.com>
2023-09-01 14:05:30 +0330

    add db connection

---
+++ dcrm/mydb.py
@@ -0,0 +1,18 @@
+# this is for once, run it and all done
+
+import mysql.connector
+
+dataBase = mysql.connector.connect(
+    host= 'localhost',
+    user= 'root',
+    passwd='mistrs2P',
+
+)
+
+# prepare cursor object
+cursorObject = dataBase.cursor()
+
+# create a database
+cursorObject.execute("CREATE DATABASE dcrm")
+
+# All done
